0c99e7f39decff8f981c1325e48a45d983abfbff
leeya018 <leeyahav018@gmail.com>
2021-10-06 19:53:01 +0300

    steirneitns

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -7,41 +7,17 @@ function Test() {
  
   return (
     <>
-      <BarcodeScannerComponent
+      {/* <BarcodeScannerComponent
         width={500}
         height={500}
         onUpdate={(err, result) => {
           if (result) setData(result.text)
           else setData('Not Found')
         }}
-      />
+      /> */}
       <p>{data}</p>
     </>
   )
 }
  
 export default Test;
-// import React, { useState } from 'react'
-// import BarcodeReader from 'react-barcode-reader'
-
-// export default function() {
-//   const [result, setResult] = useState('No result')
-
-//   function handleScan(data){
-//     setResult(data)
-//   }
-//  function handleError(err) {     
-//      console.error(err)
-//  }
-
-//     return(
-//       <div>
-//         <BarcodeReader
-//           onError={handleError}
-//           onScan={handleScan}
-//           />
-//         <p>{result}</p>
-//       </div>
-//     )
-//   }
-
