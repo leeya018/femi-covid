ea8df73cef6496a4962ab5730e4e42056c26f450
lee yahav <leeyahav018@gmail.com>
2022-03-05 01:34:14 +0200

    wage for num of tests and total money

--- src/components/MonthlySalary.jsx
+++ src/components/MonthlySalary.jsx
@@ -7,15 +7,24 @@ const URER_ID = '300628583'
 const WORKER_ID = '8184'
 const HOUR_RATE = 29
 const TRANSFER_RATE = 40
+const RATE_PER_TEST = 3
+
+
 
 
 
 export default function MonthlySalary() {
-    const [month, setMonth] = useState(2)
-    const [year, setYear] = useState(2022)
+    let currDate = new Date()
+    let currMonth = currDate.getMonth() + 1
+    let currYear = currDate.getFullYear()
+
+    const [month, setMonth] = useState(currMonth)
+    const [year, setYear] = useState(currYear)
 
     const [wageForTime, setWageForTime] = useState()
     const [wageForTransfer, setWageForTransfer] = useState()
+    const [wageForTests, setWageForTests] = useState()
+
 
 
 
@@ -92,9 +101,7 @@ export default function MonthlySalary() {
     }
 
     async function calcMoneyForHours() {
-        if (localStorage.getItem("isufitToken") == null) {
-            await login()
-        }
+ 
         let timeInHours = await getWorkerAttendance()
         setWageForTime(timeInHours * HOUR_RATE)
 
@@ -136,7 +143,28 @@ export default function MonthlySalary() {
         return countOfTransfer
     }
 
-    async function getInstituteChangeNum() {
+    function fliterMe(arr) {
+        return arr.filter(client => [2, 3, 4].includes(client.status));
+
+    }
+
+    async function getValidClients(coordsId) {
+        let res = await api.getClients(coordsId)
+        return fliterMe(res.data)
+    }
+
+    async function sumAmountOfTests(monthsRecepions) {
+
+        return await monthsRecepions.reduce(async (countClients, rec) => {
+            let validClients = await getValidClients(rec.id)
+            return (await countClients) + validClients.length
+        }, 0)
+
+
+
+    }
+
+    async function calcBonus() {
         let res = await api.getAllReceptions()
         let receptions = res.data
         console.log(receptions)
@@ -146,16 +174,40 @@ export default function MonthlySalary() {
         let amountOfTransfers = getAmountOfChanges(monthsRecepions)
         console.log(amountOfTransfers)
         setWageForTransfer(amountOfTransfers * TRANSFER_RATE)
+
+        // tests in general 
+        let amountOfTests = await sumAmountOfTests(monthsRecepions)
+        console.log(amountOfTests)
+        setWageForTests(amountOfTests * RATE_PER_TEST)
     }
 
-    
+    async function calc(){
+        if (localStorage.getItem("isufitToken") == null) {
+            await login()
+        }
+
+        await calcMoneyForHours()
+        await calcBonus()
+    }
+
+
     return (
 
         <div>
             <h1>Monthly Salary</h1>
-            <button onClick={getInstituteChangeNum}>calc</button>
+            <label htmlFor="">month</label><input type="number" value={month} onChange={e=>setMonth(parseInt(e.target.value))}/><br />
+            <label htmlFor="">year</label><input type="number" value={year} onChange={e=>setYear(parseInt(e.target.value))}/><br />
+
+            <button onClick={calc}>calc</button>
             {wageForTime && <span>money for hour: {wageForTime.toFixed()}</span>}
+            <br />
             {wageForTransfer && <span>money for transfer: {wageForTransfer}</span>}
+            <br />
+            {wageForTests && <span>money for tests: {wageForTests}</span>}
+            <br />
+            {wageForTime && wageForTransfer && wageForTests && <span>total money per month: {wageForTests + wageForTransfer + wageForTime}</span>}
+
+
 
         </div>
     )
